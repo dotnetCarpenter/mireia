53233a2f270717835eea283903cbbe23a09a7b91
Jon Ege Ronnenberg <jon.ronnenberg@gmail.com>
2012-10-29 12:53:56 -0400

    ready to play with masonry and isotope

--- newsite/scripts/style.js
+++ newsite/scripts/style.js
@@ -1,13 +1,32 @@
 var ns = {}
-ns.growAside = function(img) {
+ns.growAside = function($, img) {
     $('aside').height(
         $(img).height()
     )
 }
+ns.layoutCollage = function($) {
+    var $container = $('.content')
+    $container.imagesLoaded(function() {
+        if($container.masonry) {
+            $container.masonry({
+                // options
+                itemSelector : 'img'
+            })
+        } else if($container.isotope) {
+            $container.isotope()
+        }
+    });
+}
 
 jQuery(function($){
-    $('#background-image').load(function() {
-        ns.growAside(this)
-        
-    })
+    var $body = $('body')
+    if($body.hasClass('about_us')) {
+        $('#background-image').load(function() {
+            ns.growAside($, this)
+            
+        })
+    }
+    if($body.hasClass('collections')) {
+        ns.layoutCollage($)
+    }
 });
