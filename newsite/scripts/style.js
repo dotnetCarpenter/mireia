14b58a72c4ce0528f045f7be03d80df69cca07e7
dotnetCarpenter <jon.ronnenberg@gmail.com>
2012-12-09 04:37:37 +0100

    split index.html into two seperate pages

--- newsite/scripts/style.js
+++ newsite/scripts/style.js
@@ -1,10 +1,6 @@
 var ns = ns ||{}
 //ns.l = function(fn) { return function() { fn.call(fn); } }
-ns.growAside = function($, elementToMatch) {
-    $('aside').height(
-        $(elementToMatch).height()
-    );
-};
+
 ns.gcd = function(a, b) {
   return b ? ns.gcd(b, a % b) : a;
 };
@@ -68,21 +64,42 @@ ns.layoutCollage = function($) {
         }
     });*/
 };
-
+ns.growAside = function($, elementToMatch) {
+    $('aside').height(
+        $(elementToMatch).height()
+    );
+};
+/**
+ * Can be used on an image to fire a callback when the image is loaded even if the load event is never fired.
+ * @example ns.imageLoad([jquery image], [callback function]);
+ * @param {jQuery} image
+ * @param {function} cb
+ */
+ns.imageLoad = function(image, cb) {
+    var puller = setInterval(function() {
+        if(image.height() > 0) {
+            clearInterval(puller);
+            cb();
+        }
+    }, 0);
+    image.on('load', function() {
+        clearInterval(puller);
+        cb();
+    });
+};
 jQuery(function($){
     var $body = $('body');
     if($body.hasClass('about_us')) {
         var $bgImage = $('#background-image');
-        var puller = setTimeout(function() {
-            if($bgImage.height() > 0) {
-                clearTimeout(puller);
-                ns.growAside($, $bgImage);
-                $('.overlay[rel="background-image"]')
-                    .css({ height: $bgImage.height(), width: $bgImage.width() })
-                    .removeClass("hide");
-            }
-        }, 250);
+        ns.imageLoad($bgImage, function cb() {
+            ns.growAside($, $bgImage);
+            $('.overlay[rel="background-image"]')
+                .css({ height: $bgImage.height(), width: $bgImage.width() })
+                .removeClass("hide")
+            ;
+        });
     }
+
     if($body.hasClass('collections')) {
         /*var numberOfImages = 0, numberOfImagesLoaded = 0;
         $('.content .collage').each(function(i,container){
