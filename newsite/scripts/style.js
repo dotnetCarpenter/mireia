cd9c25582bb53b783309c78f6792aed813737d91
Jon Ege Ronnenberg <jon.ronnenberg@gmail.com>
2012-12-11 22:54:23 -0500

    removed unused stuff

--- newsite/scripts/style.js
+++ newsite/scripts/style.js
@@ -1,69 +1,9 @@
-var ns = ns ||{}
-//ns.l = function(fn) { return function() { fn.call(fn); } }
-
+var ns = ns || {};
+/*
 ns.gcd = function(a, b) {
   return b ? ns.gcd(b, a % b) : a;
-};
-ns.reziseImage = function(img, factor) {
-    var $img = $(img);
-    $img.height((img.naturalHeight) * Math.sqrt(factor));
-    //$img.width(img.naturalWidth*Math.sqrt(factor));
-};
-ns.sum = function(a,b){ return a+b; };
-ns.multi = function(a,b){ return a*b; };
-ns.calculateFactor = function(imagesizes) {
-    var imgSum2 = imagesizes.reduce(ns.sum),
-        containerSum2 = $('.content').width() * $(window).height();
-    return containerSum2/imgSum2;
-};
-function Rectangle(config) {
-    this.src = config && config.src;
-    this.height = config && config.height;
-    this.width = config && config.width;
-}
-Rectangle.prototype.factor = 0.0;
-Rectangle.prototype.availHeight = $(window).height();
-Rectangle.prototype.availWidth = $('.content').width();
+};*/
 
-ns.layoutCollage = function($) {
-    var img2 = [], factor = 0.0;
-    
-    function eachImg(fn) {
-        $('.content img').each(function(i, el){
-            fn.call(fn, el, i);
-        });
-    }
-    function collectImageArea(el, i) {
-        var width = el.naturalWidth, height = el.naturalHeight;
-        img2.push(width * height);
-    }
-    // collect area of images
-    eachImg(collectImageArea);
-    factor = ns.calculateFactor(img2);
-    eachImg(function(el, i) {
-        ns.reziseImage(el, factor);
-    });
-    /*
-    var $container = $('.content');
-    $container.imagesLoaded(function() {
-        if($container.masonry) {
-            $container.masonry({
-                // options
-                itemSelector : '.collage'
-            });
-        } else if($container.isotope) {
-            $container.isotope({
-                columnWidth: $container.width() / 4
-              , sortBy: 'random'
-            });
-            $(window).smartresize(function() {
-                $container.isotope({
-                    columnWidth: $container.width() / 4
-                });
-            });
-        }
-    });*/
-};
 ns.growAside = function($, elementToMatch) {
     $('aside').height(
         $(elementToMatch).height()
